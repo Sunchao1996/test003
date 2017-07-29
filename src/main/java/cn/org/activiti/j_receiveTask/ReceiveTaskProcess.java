713e135079305933b927a306ce9e4c554bbb8bab
Sunc <2061857679@qq.com>
2017-07-29 23:36:04 +0800

    r

---
+++ src/main/java/cn/org/activiti/j_receiveTask/ReceiveTaskProcess.java
@@ -0,0 +1,16 @@
+package cn.org.activiti.j_receiveTask;
+
+import org.activiti.engine.ProcessEngine;
+import org.activiti.engine.ProcessEngines;
+import org.junit.Test;
+
+/**
+ * Created by Administrator on 2017/7/29.
+ */
+public class ReceiveTaskProcess {
+    private ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+    @Test
+    public void delpoy(){
+
+    }
+}
